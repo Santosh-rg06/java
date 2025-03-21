55d4c079510b94676028b120f3e1bf3238a4ded1
Santosh-rg06 <santumorakhandi14@gmail.com>
2025-03-21 15:26:19 +0530

    pushing all the code to git

---
+++ ProductFinder.java
@@ -0,0 +1,86 @@
+class ProductFinder {
+    static void searchProductAvailability(String productName) {
+        if (productName == "watch") {
+            System.out.println("Available");
+            return;
+        } else if (productName == "pen") {
+            System.out.println("Available");
+            return;
+        } else if (productName == "shirt") {
+            System.out.println("Available");
+            return;
+        } else if (productName == "laptop") {
+            System.out.println("Available");
+            return;
+        } else if (productName == "mobile") {
+            System.out.println("Available");
+            return;
+        } else if (productName == "headphones") {
+            System.out.println("Available");
+            return;
+        } else if (productName == "slipper") {
+            System.out.println("Available");
+            return;
+        } else if (productName == "shoes") {
+            System.out.println("Available");
+            return;
+        } else if (productName == "goldChain") {
+            System.out.println("Available");
+            return;
+        } else if (productName == "bags") {
+            System.out.println("Available");
+            return;
+        } else if (productName == "belt") {
+            System.out.println("Available");
+            return;
+        } else if (productName == "perfume") {
+            System.out.println("Available");
+            return;
+        } else if (productName == "chips") {
+            System.out.println("Available");
+            return;
+        } else if (productName == "cookies") {
+            System.out.println("Available");
+            return;
+        } else if (productName == "mixer") {
+            System.out.println("Available");
+            return;
+        } else if (productName == "fridge") {
+            System.out.println("Available");
+            return;
+        } else if (productName == "washingMachine") {
+            System.out.println("Available");
+            return;
+        } else if (productName == "paint") {
+            System.out.println("Available");
+            return;
+        } else if (productName == "cement") {
+            System.out.println("Available");
+            return;
+        } else if (productName == "powder") {
+            System.out.println("Available");
+            return;
+        } else if (productName == "pickle") {
+            System.out.println("Available");
+            return;
+        } else if (productName == "petrol") {
+            System.out.println("Available");
+            return;
+        } else if (productName == "motorcycle") {
+            System.out.println("Available");
+            return;
+        } else if (productName == "helmet") {
+            System.out.println("Available");
+            return;
+        } else if (productName == "sofa") {
+            System.out.println("Available");
+            return;
+        } else if (productName == "table") {
+            System.out.println("Available");
+            return;
+        } else {
+            System.out.println("The product is not available");
+            return;
+        }
+    }
+}
